4611bbeb29e61ad588edfa59daab5a542a886270
Kachow <karsab343@gmail.com>
2022-07-25 20:32:15 -0700

    Prints to console

--- action.js
+++ action.js
@@ -1,37 +1,18 @@
-document.getElementById('fillForm').addEventListener('click', function(){
-    chrome.tabs.executeScript({
-      file: "injector.js"
-    });
-    var test = document.getElementById("text").value;
-    var test2 = document.getElementById("text2").value;
-    var test3 = document.getElementById("text3").value;
-    var test4 = document.getElementById("text4").value;
-    var test5 = document.getElementById("text5").value;
-    var test6 = document.getElementById("text6").value;
-    var test7 = document.getElementById("text7").value;
-    var test8 = document.getElementById("text8").value;
-    var year = new Date();
-    var age = year.getFullYear();
+document.getElementById('doAction').addEventListener('click', function(){
+  chrome.tabs.executeScript({
+    file: "injector.js"
+  }, function (result) {
     
-    var ageCalculated = age - test7;
-    chrome.storage.local.set({yourValue: test}, function(){});
-    chrome.storage.local.set({yourValue2: test2}, function(){});
-    chrome.storage.local.set({yourValue3: test3}, function(){});
-    chrome.storage.local.set({yourValue4: test4}, function(){});
-    chrome.storage.local.set({yourValue5: test5}, function(){});
-    chrome.storage.local.set({yourValue6: test6}, function(){});
-    chrome.storage.local.set({yourValue7: test7}, function(){});
-   chrome.storage.local.set({yourValue8: test8}, function(){});
-   chrome.storage.local.set({ageCalculated: ageCalculated}, function(){});
+    document.getElementById("channel-name").innerHTML = result[0];
+    console.log(result[0]);
   });
+});
 
-
-
-var a = document.getElementById('media');
-a.addEventListener('change', function() {
- var media = this.value;
- chrome.storage.local.set({Media: media}, function(){});
-
-}, false);
-  
-  
+var channel = document.getElementById("channel-name");
+var links = document.getElementById("links");
+console.log(channel);
+function returnArray(resultsArray){
+  console.log(resultsArray);
+  document.getElementById("channel_name").innerHTML = resultsArray[0];
+  document.getElementById("links").innerHTML = resultsArray[1];
+}
--- injector.js
+++ injector.js
@@ -1,54 +1,13 @@
-chrome.storage.local.get(['yourValue', 'yourValue2', 'yourValue3', 'yourValue4', 'yourValue5', 'yourValue6', 'yourValue7', 'Media', 'yourValue8', 'ageCalculated'], function (result) {
-    // your value is in result.yourValue
+chrome.storage.local.get(['values',], function (result) {
+    var channel_name = document.querySelectorAll('yt-formatted-string[class="style-scope ytd-channel-name"]')[0].innerText;
+    var links = document.getElementsByClassName("yt-simple-endpoint style-scope ytd-grid-video-renderer");
+    var values = [].map.call(links, function (el) {
+        return "youtube.com" + el.getAttribute("href");
+    });
 
-    if (result.Media === "Facebook") {
-        document.querySelector('[name="firstname"]').value = result.yourValue;
-        document.querySelector('[name="lastname"]').value = result.yourValue2;
-        document.querySelector('[name="reg_email__"]').value = result.yourValue3;
-        document.querySelector('[name="reg_passwd__"]').value = result.yourValue4;
-        document.querySelector('[name="birthday_month"]').value = result.yourValue5;
-        document.querySelector('[name="birthday_day"]').value = result.yourValue6;
-        document.querySelector('[name="birthday_year"]').value = result.yourValue7;
-    }
+    console.log(channel_name);
+    console.log(values);
 
-    if (result.Media === "Instagram") {
-
-        document.querySelector('[name="emailOrPhone"]').value = result.yourValue3;
-        document.querySelector('[name="fullName"]').value = result.yourValue + " " + result.yourValue2;
-        document.querySelector('[name="username"]').value = result.yourValue8;
-        document.querySelector('[name="password"]').value = result.yourValue4;
-    }
-
-    if (result.Media === "Twitter") {
-
-
-        document.querySelector('[name="name"]').value = result.yourValue + " " + result.yourValue2;
-        document.querySelector('[name="email"]').value = result.yourValue3;
-        document.getElementById("SELECTOR_1").value = result.yourValue5;
-        document.getElementById("SELECTOR_2").value = result.yourValue6;
-        document.getElementById("SELECTOR_3").value = result.yourValue7;
-    }
-
-    if (result.Media === "LinkedIn") {
-        document.querySelector('[name="email-address"]').value = result.yourValue3;
-        document.querySelector('[name="password"]').value = result.yourValue4;
-    }
-
-    if (result.Media === "Pinterest") {
-      
-        document.querySelector('[name="id"]').value = result.yourValue3;
-        document.querySelector('[name="password"]').value = result.yourValue4;
-
-        document.querySelector('[name="age"]').value = result.ageCalculated;
-    }
-
-
-
-
-
-
-
-
-
-
+    var return_array = [channel_name, values];
+    return return_array;
 });
