6cf587e8753ea874908d01713e46a97153a971ac
Kachow <karsab343@gmail.com>
2022-07-25 23:57:25 -0700

    added img, channel name, link

--- action.js
+++ action.js
@@ -1,18 +1,37 @@
 document.getElementById('doAction').addEventListener('click', function(){
-  chrome.tabs.executeScript({
-    file: "injector.js"
-  }, function (result) {
-    
-    document.getElementById("channel-name").innerHTML = result[0];
-    console.log(result[0]);
-  });
+    document.getElementById('doAction').style = "display:none";
+    chrome.tabs.executeScript({
+      file: "content-script.js"
+    });
+    chrome.storage.local.get(['output_img', 'output_name', 'output', 'output_length'], function (result) {
+
+      var channel = document.getElementById("channel-name");  
+      var links = document.getElementById("links");
+
+      var imgs = document.getElementById('imgs');
+      var pic = document.createElement('img');
+      var picsrc = result.output_img;
+      pic.src = picsrc;
+      console.log(picsrc);
+      imgs.appendChild(pic);
+
+      channel.innerHTML = result.output_name;
+      console.log(result.output_length);
+      for(var i=0; i<result.output_length; i+=1){
+        var currLink = result.output[i];
+        // console.log(currLink)
+        var a = document.createElement('a');
+        a.href = currLink;
+        a.innerHTML = currLink;
+        links.appendChild(a);
+        // console.log(a);
+      }
+
+
+      // console.log(channel);
+      // console.log(links);
+    });
 });
 
-var channel = document.getElementById("channel-name");
-var links = document.getElementById("links");
-console.log(channel);
-function returnArray(resultsArray){
-  console.log(resultsArray);
-  document.getElementById("channel_name").innerHTML = resultsArray[0];
-  document.getElementById("links").innerHTML = resultsArray[1];
-}
+// var links = document.getElementById("links");
+
